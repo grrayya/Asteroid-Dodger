9d8d2f264bef4b704ff33b6d31bdf97af92efcf5
grrayya <grrayya11@gmail.com>
2026-07-07 23:52:51 -0700

    Add reset_game method and update game logic

--- dodger.py
+++ dodger.py
@@ -14,6 +14,90 @@ class DodgerGame:
         curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)  # UI / Text
         
         self.reset_game()
+ def reset_game(self):
+        self.stdscr.clear()
+        self.score = 0
+        self.speed = 100         # ms delay per frame
+        self.spawn_rate = 10     # % chance to spawn an asteroid per frame
+        self.paused = False
+        self.game_over = False
+        
+        # Player starting position (bottom center)
+        self.player_x = self.sw // 2
+        self.player_y = self.sh - 2
+        self.ship_sprite = "<A>" # The player is 3 characters wide
+        
+        # List to track all active asteroids: [y, x]
+        self.asteroids = []
+
+   
+
+    def draw(self):
+        self.stdscr.clear()
+        
+        # Draw Border & Score
+        self.stdscr.attron(curses.color_pair(3))
+        self.stdscr.border(0)
+        self.stdscr.addstr(0, 2, f" Score: {self.score} ")
+        if self.paused:
+            self.stdscr.addstr(self.sh // 2, self.sw // 2 - 4, " PAUSED ")
+        self.stdscr.attroff(curses.color_pair(3))
+
+        # Draw Asteroids
+        self.stdscr.attron(curses.color_pair(2))
+        for ay, ax in self.asteroids:
+            try:
+                self.stdscr.addch(ay, ax, '*')
+            except curses.error:
+                pass
+        self.stdscr.attroff(curses.color_pair(2))
+
+        # Draw Player Ship
+        self.stdscr.attron(curses.color_pair(1))
+        try:
+            # We draw starting at x - 1 because the center coordinate is x
+            self.stdscr.addstr(self.player_y, self.player_x - 1, self.ship_sprite)
+        except curses.error:
+            pass
+        self.stdscr.attroff(curses.color_pair(1))
+        
+        self.stdscr.refresh()
+
+    def show_game_over_screen(self):
+        self.stdscr.clear()
+        self.stdscr.attron(curses.color_pair(3))
+        self.stdscr.border(0)
+        
+        msg1 = " CRASHED! "
+        msg2 = f" Final Score: {self.score} "
+        msg3 = " Press 'R' to Restart or 'Q' to Quit "
+        
+        self.stdscr.addstr(self.sh // 2 - 1, (self.sw - len(msg1)) // 2, msg1)
+        self.stdscr.addstr(self.sh // 2, (self.sw - len(msg2)) // 2, msg2)
+        self.stdscr.addstr(self.sh // 2 + 1, (self.sw - len(msg3)) // 2, msg3)
+        self.stdscr.attroff(curses.color_pair(3))
+        self.stdscr.refresh()
+        
+        while True:
+            key = self.stdscr.getch()
+            if key in [ord('r'), ord('R')]:
+                self.reset_game()
+                break
+            elif key in [ord('q'), ord('Q')]:
+                self.game_over = True
+                break
+
+    def run(self):
+        curses.curs_set(0)
+        self.stdscr.nodelay(1)
+        
+        while not self.game_over:
+            self.stdscr.timeout(self.speed)
+            self.process_input()
+            if not self.game_over:
+                self.update_logic()
+            if not self.game_over:
+                self.draw()
 
    
 def main(stdscr):
