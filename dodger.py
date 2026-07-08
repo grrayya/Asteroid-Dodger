72a9bff70041541b6fc248b165a9b49ffe45266a
grrayya <grrayya11@gmail.com>
2026-07-07 23:56:55 -0700

    Update dodger.py

--- dodger.py
+++ dodger.py
@@ -1,5 +1,6 @@
 import curses
 import random
+import time
 
 class DodgerGame:
     def __init__(self, stdscr):
@@ -12,6 +13,7 @@ class DodgerGame:
         curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)    # Asteroids
         curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)  # UI / Text
         curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK) # Lasers
+        curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_BLACK)  # Power-ups
         
         self.reset_game()
 
@@ -28,7 +30,12 @@ class DodgerGame:
         self.ship_sprite = "<A>" 
         
         self.asteroids = []
-        self.lasers = []  # New: Track active lasers
+        self.lasers = []
+        self.max_lasers = 3  
+        
+        self.powerups = []           
+        self.powerup_active = False  
+        self.powerup_end_time = 0    
 
     def process_input(self):
         next_key = self.stdscr.getch()
@@ -46,9 +53,17 @@ class DodgerGame:
                 self.player_x = max(2, self.player_x - 2)
             elif next_key == curses.KEY_RIGHT:
                 self.player_x = min(self.sw - 4, self.player_x + 2)
-            elif next_key == ord(' '):  # New: Fire laser on spacebar
-                # Spawn laser just above the center of the ship
-                self.lasers.append([self.player_y - 1, self.player_x])
+            elif next_key == ord(' '):  
+                if self.powerup_active:
+                    # Allow up to 9 lasers on screen during Spread Shot
+                    if len(self.lasers) <= 6:
+                        self.lasers.append([self.player_y - 1, self.player_x - 1]) # Left
+                        self.lasers.append([self.player_y - 1, self.player_x])     # Center
+                        self.lasers.append([self.player_y - 1, self.player_x + 1]) # Right
+                else:
+                    # Standard single shot
+                    if len(self.lasers) < self.max_lasers:
+                        self.lasers.append([self.player_y - 1, self.player_x])
 
     def update_logic(self):
         if self.paused:
@@ -58,7 +73,7 @@ class DodgerGame:
         surviving_lasers = []
         for ly, lx in self.lasers:
             ly -= 1
-            if ly > 0:  # Keep if it hasn't hit the top of the screen
+            if ly > 0:  
                 surviving_lasers.append([ly, lx])
         self.lasers = surviving_lasers
 
@@ -67,7 +82,6 @@ class DodgerGame:
         for ay, ax in self.asteroids:
             ay += 1 
             
-            # Check collision with player
             if ay == self.player_y and (self.player_x - 1 <= ax <= self.player_x + 1):
                 self.game_over = True
                 return
@@ -92,21 +106,41 @@ class DodgerGame:
                 if i in lasers_to_remove: 
                     continue
                 
-                # Check for direct hit OR "tunneling" (they crossed paths this frame)
+                # Check for direct hit OR "tunneling"
                 if lx == ax and (ly == ay or ly == ay - 1):
                     destroyed = True
                     lasers_to_remove.add(i)
-                    self.score += 2  # Bonus points for destroying an asteroid
+                    self.score += 2  
+                    
+                    # 15% chance to drop a 'Spread Shot' power-up
+                    if random.random() < 0.15:
+                        self.powerups.append([ay, ax])
                     break
                     
             if not destroyed:
                 new_asteroids.append([ay, ax])
                 
-        # Update lists after collisions
         self.asteroids = new_asteroids
         self.lasers = [l for i, l in enumerate(self.lasers) if i not in lasers_to_remove]
         
-        # 4. Spawn new asteroids
+        # 4. Move power-ups and check player collision
+        surviving_powerups = []
+        for py, px in self.powerups:
+            py += 1
+            if py == self.player_y and (self.player_x - 1 <= px <= self.player_x + 1):
+                # Player caught it! Activate the buff for 5 seconds
+                self.powerup_active = True
+                self.powerup_end_time = time.time() + 5.0 
+                self.score += 5
+            elif py < self.sh - 1:
+                surviving_powerups.append([py, px])
+        self.powerups = surviving_powerups
+
+        # Turn off the buff if the timer runs out
+        if self.powerup_active and time.time() > self.powerup_end_time:
+            self.powerup_active = False
+        
+        # 5. Spawn new asteroids
         if random.randint(1, 100) <= self.spawn_rate:
             spawn_x = random.randint(1, self.sw - 2)
             self.asteroids.append([1, spawn_x])
@@ -114,15 +148,24 @@ class DodgerGame:
     def draw(self):
         self.stdscr.clear()
         
-        # Draw Border & Score
+        # Draw Border, Score, and Ammo
         self.stdscr.attron(curses.color_pair(3))
         self.stdscr.border(0)
+        
         self.stdscr.addstr(0, 2, f" Score: {self.score} ")
+        
+        # Ammo Gauge
+        if self.powerup_active:
+            self.stdscr.addstr(0, 15, " Shots: [ SPREAD ] ")
+        else:
+            available_ammo = self.max_lasers - len(self.lasers)
+            self.stdscr.addstr(0, 15, f" Shots: {'|' * available_ammo:<3} ")
+            
         if self.paused:
             self.stdscr.addstr(self.sh // 2, self.sw // 2 - 4, " PAUSED ")
         self.stdscr.attroff(curses.color_pair(3))
 
-        # Draw Lasers (New)
+        # Draw Lasers 
         self.stdscr.attron(curses.color_pair(4))
         for ly, lx in self.lasers:
             try:
@@ -130,6 +173,15 @@ class DodgerGame:
             except curses.error:
                 pass
         self.stdscr.attroff(curses.color_pair(4))
+
+        # Draw Power-ups
+        self.stdscr.attron(curses.color_pair(5))
+        for py, px in self.powerups:
+            try:
+                self.stdscr.addch(py, px, 'W')
+            except curses.error:
+                pass
+        self.stdscr.attroff(curses.color_pair(5))
 
         # Draw Asteroids
         self.stdscr.attron(curses.color_pair(2))
